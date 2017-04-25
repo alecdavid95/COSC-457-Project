85db2011ea39622378c40e1abb65e196cf752c99
Sean Donnelly <sdonnelly3000@gmail.com>
2017-04-25 13:32:36 -0400

    Migrate Template Engine to JSX
    The template engine has been switched from Jade to JSX using React's
    express-react-views library. This does not mount react components on the
    client, but rather renders static pages to be returned to the client.

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 var express = require('express')
 var app = express()
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+app.get('/', function(req, res){
+  res.render('index', { name: 'Group 3' });
+});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
