6b3b94b31e328a49231d58773bc74408d9ef1364
Sean Donnelly <sdonnelly3000@gmail.com>
2017-04-27 11:18:39 -0400

    Create Angular Client
    A client directory has been created that houses an Angular application.
    The server redirects any route besides /api to this client.
    
    A routes directory has also been made, this is where all further routes
    for the server should be defined.

--- app.js
+++ app.js
@@ -1,10 +1,41 @@
-var express = require('express')
-var app = express()
+// Get dependencies
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const bodyParser = require('body-parser');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+// Get our API routes
+const api = require('./routes/api');
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+const app = express();
+
+// Parsers for POST data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Point static path to dist
+app.use(express.static(path.join(__dirname, 'client/dist')));
+
+// Set our api routes
+app.use('/api', api);
+
+// Catch all other routes and return the index file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/dist/index.html'));
+});
+
+/**
+ * Get port from environment and store in Express.
+ */
+const port = process.env.PORT || '3000';
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+const server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+server.listen(port, () => console.log(`API running on localhost:${port}`));
