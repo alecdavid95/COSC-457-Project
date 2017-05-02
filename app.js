873e69644104150b9c61b29303bcdf6dac834377
Kevin Boyd <kevin.boyd34@comcast.net>
2017-05-01 23:33:22 -0400

    Update API with get requests
    Update API with select all queries for all entities

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ pool.on('error', function (err) {
     console.log(err);
 });
 
+//----------------------------------------------------------------------------------
 // Middleware
 //----------------------------------------------------------------------------------
 // configure app to use bodyParser()
@@ -75,9 +76,129 @@ pool.acquire(function (err, connection) {
     router.get('/', function (req, res) {
         res.json({message: 'welcome to big red booking api'});
     });
+    // ACTS
+    router.get('/acts', function (req, res) {
+        var request = new Request('SELECT * FROM [Acts];',
+            function (err, rowCount, rows) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    res.json(rows);
+                }
+            });
+        connection.execSql(request);
+    });
 
-    router.get('/test', function (req, res) {
-        var request = new Request('SELECT * FROM [TestTable];',
+    // COSTS
+    router.get('/costs/:show_id', function (req, res) {
+        var showID = req.params['show_id'];
+        var request = new Request('SELECT * FROM [Costs] WHERE Show_ID = ' + showID +  ';',
+            function (err, rowCount, rows) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    res.json(rows);
+                }
+            });
+        connection.execSql(request);
+    });
+
+    // EMPLOYEE
+    router.get('/employees', function (req, res) {
+        var request = new Request('SELECT * FROM [Employee];',
+            function (err, rowCount, rows) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    res.json(rows);
+                }
+            });
+        connection.execSql(request);
+    });
+
+    // EQUIPMENT
+    router.get('/equipment/:venue_id', function (req, res) {
+        var venueID = req.params['venue_id'];
+        var request = new Request('SELECT * FROM [Equipment] WHERE Ven_ID = ' + venueID + ' ;',
+            function (err, rowCount, rows) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    res.json(rows);
+                }
+            });
+        connection.execSql(request);
+    });
+
+    // HOUSING
+    router.get('/housing/:venue_id', function (req, res) {
+        var venueID = req.params['venue_id'];
+        var request = new Request('SELECT * FROM [Acts] WHERE Ven_ID = ' + venueID + ';',
+            function (err, rowCount, rows) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    res.json(rows);
+                }
+            });
+        connection.execSql(request);
+    });
+
+    // PROMOTER
+    router.get('/promoters', function (req, res) {
+        var request = new Request('SELECT * FROM [Promoter];',
+            function (err, rowCount, rows) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    res.json(rows);
+                }
+            });
+        connection.execSql(request);
+    });
+
+    // SHOWS
+    router.get('/shows', function (req, res) {
+        var request = new Request('SELECT * FROM [Shows];',
+            function (err, rowCount, rows) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    res.json(rows);
+                }
+            });
+        connection.execSql(request);
+    });
+
+    // TICKETS
+    router.get('/tickets', function (req, res) {
+        var request = new Request('SELECT * FROM [Tickets];',
+            function (err, rowCount, rows) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    res.json(rows);
+                }
+            });
+        connection.execSql(request);
+    });
+
+    // TOURS
+    router.get('/tours', function (req, res) {
+        var request = new Request('SELECT * FROM [Tours];',
+            function (err, rowCount, rows) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    res.json(rows);
+                }
+            });
+        connection.execSql(request);
+    });
+
+    // VENUE
+    router.get('/venues', function (req, res) {
+        var request = new Request('SELECT * FROM [Venue];',
             function (err, rowCount, rows) {
                 if (err) {
                     console.log(err);
